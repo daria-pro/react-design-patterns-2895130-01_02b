d8b2e2234a503a5ea9f6bd34b191f98d6dba4119
daria-pro <shaulkod@gmail.com>
2023-09-03 10:35:19 +0300

    Modal components

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 01_03: Split-screen component improvements
 01_04: Lists and list items
 01_05: Creating different list types
+01_06: Modal components
--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import RegularList from "./RegularList";
-import NumberedList from "./NumberedList";
-import { people, products } from "./constants";
-import LargePersonListItem from "./people/LargePersonListItem";
-import SmallPersonLIstItem from "./people/SmallPersonLIstItem";
+import { products } from "./constants";
 import LargeProductListItem from "./products/LargeProductListItem";
+import { Modal } from "./Modal";
 
 function App() {
   return (
     <>
-      <RegularList
-        items={people}
-        resourceName="person"
-        itemComponent={SmallPersonLIstItem}
-      />
-      <RegularList
-        items={people}
-        resourceName="person"
-        itemComponent={LargePersonListItem}
-      />
-      <NumberedList
-        items={products}
-        resourceName="product"
-        itemComponent={LargeProductListItem}
-      />
+      <Modal>
+        <LargeProductListItem product={products[0]} />
+      </Modal>
     </>
   );
 }
