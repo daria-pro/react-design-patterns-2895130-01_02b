fa5e1ba3ca05e3134ec5dbc5566847d71ede1575
daria-pro <shaulkod@gmail.com>
2023-09-11 09:42:20 +0300

    fix server request

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import { UserInfo } from "./UserInfo";
 
 function App() {
   return (
-    <UserLoader userId={234}>
-      <UserInfo />
-    </UserLoader>
+    <>
+      <UserLoader userId={123}>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId={234}>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId={345}>
+        <UserInfo />
+      </UserLoader>
+    </>
   );
 }
 export default App;
--- src/UserLoader.js
+++ src/UserLoader.js
@@ -1,12 +1,13 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const UserLoader = ({ userId, children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const response = await axios.get(`/users/:${userId}`);
+      const response = await axios.get(`/users/${userId}`);
+      console.log(response);
       setUser(response.data);
     })();
   }, [userId]);
