6c4e5db32e25ebd411b75d47c6a372b0711e1a45
daria-pro <shaulkod@gmail.com>
2023-09-14 22:43:03 +0300

    Uncontrolled forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import { SplitScreen } from "./components/SplitScreen";
-
-const LeftHandComponent = () => {
-  return <h1>Left!</h1>;
-};
-
-const RightHandComponent = () => {
-  return <h1>Right!</h1>;
-};
+import UncontrolledForm from "./UncontrolledForm";
 
 function App() {
-  return <SplitScreen left={LeftHandComponent} right={RightHandComponent} />;
+  return <UncontrolledForm />;
 }
 
 export default App;
